75be69591e06900eabcfd7f547ab3026f1ea1b90
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2023-12-06 20:46:38 -0300

    Exercicio Javascript parte 3

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -9,11 +9,43 @@ function verificar() {
      var fsex = document.getElementsByName('radsex')
      var idade = ano - Number(fano.value)
      var genero = ''
+     var img = document.createElement('img')
+     img.setAttribute('id', 'foto')
      if (fsex[0].checked) {
         genero = 'Homem'
-     } else if (fsex[1].checked) {
+        if (idade >=0 && idade < 10) {
+            //criança
+            img.setAttribute('src', 'bebe-homem.png' )
+        } else if (idade< 21) {
+            // jovem
+            img.setAttribute('src', 'homem-jovem.png')
+        } else if (idade < 50) {
+            // adulto
+            img.setAttribute('src', 'homem-adulto.png')
+        } else {
+            // idoso
+            img.setAttribute('src', 'idoso-homem.png')
+        }
+            
         
+     } else if (fsex[1].checked) {
+        genero = 'Mulher'
+        if (idade >=0 && idade < 10) {
+            //criança
+            img.setAttribute('src', 'bebe-mulher.png' )
+        } else if (idade< 21) {
+            // jovem
+            img.setAttribute('src', 'jovem-mulher.png' )
+        } else if (idade < 50) {
+            // adulto
+            img.setAttribute('src', 'mulher-adulta.png' )
+        } else {
+            //idoso
+            img.setAttribute('src', 'idosa-mulher.png' )
+        }
      }
-    
+     res.style.textAlign = `center`
+     res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+     res.appendChild(img)
    }
 }
